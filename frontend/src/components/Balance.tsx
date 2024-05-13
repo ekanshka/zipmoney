49a1fc04d26f448b8390d1f0a346432f2d374159
Ekansh <efuforofficial@gmail.com>
2024-05-13 20:06:33 +0530

    made balance component

--- frontend/src/components/Balance.tsx
+++ frontend/src/components/Balance.tsx
@@ -1,16 +1,35 @@
+import axios from "axios";
 import { useEffect, useState } from "react"
 
 
 export const Balance = () => {
 
-  const [balance, setBalance] = useState()
+  const [balance, setBalance] = useState("");
+
+  const getBalance = async () => {
+    const response = await axios.get("http://localhost:3000/api/v1/account/balance", {
+      headers: {
+        Authorization: localStorage.getItem("authorization")
+      }
+    })
+
+    if (response.data.balance) {
+      setBalance(response.data.balance);
+    } 
+  }
+
+  useEffect(() => {
+    getBalance();
+  }, [balance])
+
+
 
   useEffect
 
   return (
-    <div className="w-[95%] p-4 rounded-2xl border font-semibold">
-      Your Balance
-      <span>{balance}</span>
+    <div className="w-[95%] p-4 rounded-2xl border font-bold">
+      <span>Your Balance </span>
+      <span className="ml-4 font-semibold">Rs. {balance}</span>
     </div>
   )
 }
